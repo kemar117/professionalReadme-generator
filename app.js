ef510cb1d1e9ae212f348c3a773fbf3a70ebd09f
Master Chief <kemar7@live.com>
2020-07-30 16:18:47 -0400

    mid 9.3.5

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
+const inquirer = require('inquirer');
+
+inquirer
+  .prompt([
+      {
+          type: 'input',
+          name: 'name',
+          message: 'What is your name?'
+      }
+  ])
+  .then(answers => console.log(answers));
+  
+/*
 const fs = require('fs');
 
 const profileDataArgs = process.argv.slice(2, process.argv.length);
@@ -5,7 +18,7 @@ const profileDataArgs = process.argv.slice(2, process.argv.length);
 const [name, github] = profileDataArgs;
 
 const generatePage = require('./src/page-template.js');
-/*
+
 const printProfileData = (profileDataArr) => {
     for (let i = 0; i < profileDataArr.length; i += 1){
 
@@ -19,10 +32,11 @@ const printProfileData = (profileDataArr) => {
 
 
 printProfileData(profileDataArgs);
-*/
+
 
 fs.writeFile('./index.html', generatePage(name, github), err => {
     if (err) throw new Error(err);
 
     console.log('Portfolio complete! Check out index.html to see the output!');
 });
+*/
